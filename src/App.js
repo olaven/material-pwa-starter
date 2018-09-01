4a578d28b7b072c9a2105685c95bbf97550432a9
olaven <olavsundfoer@gmail.com>
2018-09-01 21:24:30 +0200

    the layout component is now its own npm package! (olaven/material-app-layout)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
 import React, { Component } from 'react';
 
-import Layout from './Components/Layout'; 
+import Layout from 'material-app-layout'; 
 
-
-class App extends Component {
+export default class App extends Component {
   render() {
-    return (
-      <div>
-        {/*
-          Add top, menu and main as props to Layout.
-          They should be react components.
-          Top is in the top bar. 
-          Menu is in the side menu. 
-          Main is in the main window of app
-        */}
-        <Layout/> 
-      </div>
-    ); 
+    return <Layout />
   }
 }
-
-export default App;
